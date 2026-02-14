965d809af6e6c9401cb663be45c1c758abca9b61
rd <hopartean.radu@spectrum.ro>
2026-02-14 21:58:48 +0200

    more functions

--- src/runtime/stl/stl.h
+++ src/runtime/stl/stl.h
@@ -7,20 +7,23 @@
 #include <array>
 #include <chrono>
 #include <random>
+#include <limits>
 #include <climits>
 #include "../../runtime/memory/memory.h"
 
 struct COMPILER;
 
-#define NR_BUILTINS 3
+#define NR_BUILTINS 5
 #define FUNCTION_REGISTERS 4
 
 struct STL {
 public:
     COMPILER* comp; 
-    const std::array<std::string, NR_BUILTINS> valid_function_names = { "List", "Time", "RandRange" };
+    const std::array<std::string, NR_BUILTINS> valid_function_names = { "List", "Time", "RandRange", "GetChar", "GetType" };
     std::function<void(bool)> builtins[NR_BUILTINS];
+    
     double number_registers[FUNCTION_REGISTERS] = {0};
+    char char_registers[FUNCTION_REGISTERS] ;
 
     void init(COMPILER* compiler);
 
@@ -28,6 +31,8 @@ private:
     void List(bool standalone);
     void Time(bool standalone);
     void RandRange(bool standalone);
+    void GetChar(bool standalone);
+    void GetType(bool standalone);
 };
 
 #endif
