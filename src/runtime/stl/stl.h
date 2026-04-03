daee5b8e8fa51177152407894058c43be2e71b87
rd <hopartean.radu@spectrum.ro>
2026-04-03 19:30:48 +0300

    System() function

--- src/runtime/stl/stl.h
+++ src/runtime/stl/stl.h
@@ -13,13 +13,13 @@
 
 struct COMPILER;
 
-#define NR_BUILTINS 5
+#define NR_BUILTINS 6
 #define FUNCTION_REGISTERS 4
 
 struct STL {
 public:
     COMPILER* comp; 
-    const std::array<std::string, NR_BUILTINS> valid_function_names = { "List", "Time", "RandRange", "GetChar", "GetType" };
+    const std::array<std::string, NR_BUILTINS> valid_function_names = { "List", "Time", "RandRange", "GetChar", "GetType", "System" };
     std::function<void(bool)> builtins[NR_BUILTINS];
     
     double number_registers[FUNCTION_REGISTERS] = {0};
@@ -33,6 +33,7 @@ private:
     void RandRange(bool standalone);
     void GetChar(bool standalone);
     void GetType(bool standalone);
+    void System(bool standalone);
 };
 
 #endif
